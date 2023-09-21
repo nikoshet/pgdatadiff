c7c00fb5c8be3e710e4b8cf32563cff419d2cfc5
nikoshet <nnikitarrr@gmail.com>
2023-09-21 15:55:22 +0300

    Enforce correct schema selection on seq/tables

--- pgdatadiff/pgdatadiff.py
+++ pgdatadiff/pgdatadiff.py
@@ -8,6 +8,7 @@ from sqlalchemy.exc import NoSuchTableError, ProgrammingError
 from sqlalchemy.inspection import inspect
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.sql.schema import MetaData, Table
+import traceback
 
 
 def make_session(connection_string):
@@ -19,15 +20,16 @@ def make_session(connection_string):
 
 class DBDiff(object):
 
-    def __init__(self, firstdb, seconddb, chunk_size=10000, count_only=False, exclude_tables=""):
+    def __init__(self, firstdb, seconddb, schema, chunk_size=10000, count_only=False, exclude_tables=""):
         firstsession, firstengine = make_session(firstdb)
         secondsession, secondengine = make_session(seconddb)
         self.firstsession = firstsession
         self.firstengine = firstengine
         self.secondsession = secondsession
         self.secondengine = secondengine
-        self.firstmeta = MetaData(bind=firstengine)
-        self.secondmeta = MetaData(bind=secondengine)
+        self.schema = schema or 'public'
+        self.firstmeta = MetaData(bind=firstengine, schema=self.schema)
+        self.secondmeta = MetaData(bind=secondengine, schema=self.schema)
         self.firstinspector = inspect(firstengine)
         self.secondinspector = inspect(secondengine)
         self.chunk_size = int(chunk_size)
@@ -36,33 +38,32 @@ class DBDiff(object):
 
     def diff_table_data(self, tablename):
         try:
-            firsttable = Table(tablename, self.firstmeta, autoload=True)
-            firstquery = self.firstsession.query(
-                firsttable)
-            secondtable = Table(tablename, self.secondmeta, autoload=True)
-            secondquery = self.secondsession.query(
-                secondtable)
+            firsttable = Table(tablename, self.firstmeta, autoload=True, schema=self.schema)
+            firstquery = self.firstsession.query(firsttable)
+            secondtable = Table(tablename, self.secondmeta, autoload=True, schema=self.schema)
+            secondquery = self.secondsession.query(secondtable)
             if firstquery.count() != secondquery.count():
                 return False, f"counts are different" \
-                              f" {firstquery.count()} != {secondquery.count()}"
+                                f" {firstquery.count()} != {secondquery.count()}"
             if firstquery.count() == 0:
                 return None, "tables are empty"
             if self.count_only is True:
                 return True, "Counts are the same"
-            pk = ",".join(self.firstinspector.get_pk_constraint(tablename)[
-                              'constrained_columns'])
+            pk = ",".join(self.firstinspector.get_pk_constraint(tablename, schema=self.schema)[
+                                'constrained_columns'])
             if not pk:
                 return None, "no primary key(s) on this table." \
-                             " Comparision is not possible."
+                                " Comparison is not possible."
 
-        except NoSuchTableError:
+        except NoSuchTableError as e:
+            print(traceback.format_exc())
             return False, "table is missing"
 
         SQL_TEMPLATE_HASH = f"""
         SELECT md5(array_agg(md5((t.*)::varchar))::varchar)
         FROM (
                 SELECT *
-                FROM {tablename}
+                FROM {self.schema}.{tablename}
                 ORDER BY {pk} limit :row_limit offset :row_offset
             ) AS t;
                         """
@@ -85,13 +86,17 @@ class DBDiff(object):
         return True, "data is identical."
 
     def get_all_sequences(self):
-        GET_SEQUENCES_SQL = """SELECT c.relname FROM 
-        pg_class c WHERE c.relkind = 'S';"""
+        GET_SEQUENCES_SQL = f"""
+        SELECT c.relname AS table_name
+        FROM pg_namespace n
+        JOIN pg_class c ON n.oid = c.relnamespace
+        WHERE c.relkind = 'S' AND n.nspname='{self.schema}';
+        """
         return [x[0] for x in
                 self.firstsession.execute(GET_SEQUENCES_SQL).fetchall()]
 
     def diff_sequence(self, seq_name):
-        GET_SEQUENCES_VALUE_SQL = f"SELECT last_value FROM {seq_name};"
+        GET_SEQUENCES_VALUE_SQL = f"SELECT last_value FROM {self.schema}.{seq_name};"
 
         try:
             firstvalue = \
@@ -141,7 +146,7 @@ class DBDiff(object):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=sa_exc.SAWarning)
             tables = sorted(
-                self.firstinspector.get_table_names(schema="public"))
+                self.firstinspector.get_table_names(schema=self.schema))
             for table in tables:
                 if table in self.exclude_tables:
                     print(bold(yellow(f"Ignoring table {table}")))
@@ -162,4 +167,3 @@ class DBDiff(object):
         if failures > 0:
             return 1
         return 0
-
